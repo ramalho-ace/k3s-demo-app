122b9c7b97f7dbe623e27c89b1b7512e537fbbc6
ramalho-ace <ramalho.alan@cloud-ace.com>
2026-01-13 14:44:17 +0000

    teste atualizando app

--- app/main.py
+++ app/main.py
@@ -82,7 +82,7 @@ def index():
   <head>
     <meta charset='utf-8'/>
     <meta name="viewport" content="width=device-width,initial-scale=1" />
-    <title>Click Counter</title>
+    <title>Contador de clicks</title>
     <script src="https://cdn.tailwindcss.com"></script>
   </head>
   <body class="bg-gradient-to-br from-sky-50 to-indigo-50 min-h-screen flex items-center justify-center">
